8c949bdfa8f45fdb61b73f3905157b68041d0096
Adam <a.lister1@lancaster.ac.uk>
2018-04-15 14:29:37 -0500

    removing final hit from likelihood calculation and making fcl configurable

--- Algorithms/Bragg_negLogL_Estimator.cxx
+++ Algorithms/Bragg_negLogL_Estimator.cxx
@@ -33,6 +33,7 @@ namespace particleid{
     landauWidth_mu = p.get<double>("dEdxLandauWidthMu" , 0.07);
     landauWidth_pi = p.get<double>("dEdxLandauWidthPi" , 0.07);
     landauWidth_k  = p.get<double>("dEdxLandauWidthK"  , 0.07);
+    nHitsToDrop    = p.get<int>("NHitsToDrop", 1);
     endPointFloatShort    = p.get<double>("EndPointFloatShort", -1.0);
     endPointFloatLong     = p.get<double>("EndPointFloatLong" , 1.0);
     endPointFloatStepSize = p.get<double>("EndPointFloatStepSize", 0.05);
@@ -50,6 +51,7 @@ namespace particleid{
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] Muon dE/dx landau width   : " << landauWidth_mu << std::endl;
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] Pion dE/dx landau width   : " << landauWidth_pi << std::endl;
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] Kaon dE/dx landau width   : " << landauWidth_k  << std::endl;
+    std::cout << "[ParticleID::Bragg_negLogL_Estimator] Number of Hits to Drop: " << nHitsToDrop << std::endl;
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] End-point float long  : " << endPointFloatLong  << std::endl;
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] End-point float short : " << endPointFloatShort  << std::endl;
     std::cout << "[ParticleID::Bragg_negLogL_Estimator] End-point step size   : " << endPointFloatStepSize  << std::endl;
@@ -129,9 +131,19 @@ namespace particleid{
         size_t rr_index;
         if (forward){ // Fit tracks "forward" (i.e. in the direction they already have)
           rr_index = i_hit;
+          std::cout << rr_index << "/" << resRange.size() << std::endl;
+          if ((int)rr_index >= (int)resRange.size() - nHitsToDrop){
+            std::cout << rr_index << "forward ignored" << std::endl;
+            continue;
+          }
         }
         else{ // Fit tracks "backward"
+          std::cout << rr_index << "/" << resRange.size() << std::endl;
           rr_index = (resRange.size()-1)-i_hit;
+          if ((int)i_hit < nHitsToDrop){
+            std::cout << rr_index << "backward ignored" << std::endl;
+            continue;
+          }
         }
 
         double resrg_i = resRange.at(rr_index)+rr_shift+rr_MIPregionshift;
