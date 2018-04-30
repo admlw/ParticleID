cee4f26df00bcaf694d703ebe830c06c40ee0ad0
kduffy <kduffy@fnal.gov>
2018-04-30 15:23:14 -0500

    Add possibility to fix Gaussian width when doing the fit. Note that this code is very hacky and I've been fixing/changing things in the code in a not-very-sensible way. If you want to use this code to fit, you should read through it first and make sure it's doing the fitting in the right way (good news is it's very short so that's easy to do)

--- galleryMacro/DistributionWidthEstimator/FitLandauGaus.C
+++ galleryMacro/DistributionWidthEstimator/FitLandauGaus.C
@@ -1,21 +1,23 @@
-#include "../../Algorithms/landauGaussian.h"
+#include "../../Algorithms/LandauGaussian.h"
 
-void FitLandauGaus(std::string filename)
+void FitLandauGaus(std::string filename, bool fixGaus=false)
 {
   TF1 *langaus = new TF1("langaus", landauGaussian, 0, 10, 4);
   langaus->SetParNames("Landau width","Peak value","Normalisation","Gaussian width");
 
   gStyle->SetOptStat(0);
   gStyle->SetOptFit(1);
+  gROOT->ForceStyle();
 
   TFile *f = new TFile(filename.c_str(),"open");
   TFile *fout = new TFile("output_LanGausFit.root","recreate");
 
   // All tracks
   TCanvas *c1 = new TCanvas();
+
   TH1D *hdEdx_all = (TH1D*)f->Get("hdEdx_all");
   hdEdx_all->GetXaxis()->SetRangeUser(0,10);
-  langaus->SetParameters(0.1,1.6,100,0.1);
+  langaus->SetParameters(0.2,1.7,1000,0.1);
   hdEdx_all->Fit(langaus,"","",0.,10.);
   hdEdx_all->Draw();
   hdEdx_all->Write("hdEdx_all");
@@ -24,8 +26,12 @@ void FitLandauGaus(std::string filename)
   c1->Write("canv_hdEdx_all");
   c1->Print("fitted_hdEdx_all.pdf");
 
+  // Get Gaussian width from this fit and fix for other distributions
+  double GausWidth = fit_hdEdx_all->GetParameter(3);
 
-  langaus->SetParameters(0.1,1.6,100,0.1);
+
+  langaus->SetParameters(0.1,1.7,1000,GausWidth);
+  if (fixGaus) langaus->FixParameter(3, GausWidth);
   // True muons
   TH1D *hdEdx_Muon = (TH1D*)f->Get("hdEdx_Muon");
   if (hdEdx_Muon->Integral() > 0){
@@ -41,14 +47,15 @@ void FitLandauGaus(std::string filename)
   }
 
 
-  langaus->SetParameters(0.5,3.5,100,0.5);
+  langaus->SetParameters(0.1,1.7,1000,GausWidth);
+  if (fixGaus) langaus->FixParameter(3, GausWidth);
   // All tracks
   TH1D *hdEdx_Proton = (TH1D*)f->Get("hdEdx_Proton");
   if (hdEdx_Proton->Integral() > 0){
     hdEdx_Proton->Rebin(2);
     hdEdx_Proton->GetXaxis()->SetRangeUser(0,10);
     TCanvas *c3 = new TCanvas();
-    hdEdx_Proton->Fit(langaus,"","",0.,10.);
+    hdEdx_Proton->Fit(langaus,"","",1.5,10.);
     hdEdx_Proton->Draw();
     hdEdx_Proton->Write("hdEdx_Proton");
     TF1 *fit_hdEdx_Proton = (TF1*)langaus->Clone("fit_hdEdx_Proton");
