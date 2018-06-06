220399cd19e35a38a0873ae7f58b19f51f8ed247
Adam <a.lister1@lancaster.ac.uk>
2018-06-06 13:58:57 -0500

    addition of further variables and template fit options, and scale factors

--- Scripts/plotFromTreeHeader.h
+++ Scripts/plotFromTreeHeader.h
@@ -14,6 +14,7 @@ struct treevars{
   std::vector<double> *track_likelihood_bwd_other=nullptr;
   std::vector<double> *track_PIDA_mean=nullptr;
   std::vector<double> *track_PIDA_kde=nullptr;
+  std::vector<double> *track_PIDA_median=nullptr;
   std::vector<double> *track_depE=nullptr;
   double track_chi2mu_plane2=-9999;
   double track_chi2p_plane2=-9999;
@@ -45,6 +46,7 @@ struct treevars{
   std::vector<double> *track_Lp_0to1;
   std::vector<double> *track_Lmumip_0to1;
   std::vector<double> *track_Lmumippi_0to1;
+  std::vector<double> *track_Lmumip_0to1_nopionkaon;
   std::vector<double> *track_depE_minus_rangeE_mu;
   std::vector<double> *track_depE_minus_rangeE_p;
   std::vector<double> *track_chi2_muminusp;
@@ -63,6 +65,7 @@ void settreevars(TTree *intree, treevars *varstoset){
   intree->SetBranchAddress("track_likelihood_bwd_k"   , &(varstoset->track_likelihood_bwd_k));
   intree->SetBranchAddress("track_PIDA_mean"       , &(varstoset->track_PIDA_mean));
   intree->SetBranchAddress("track_PIDA_kde"        , &(varstoset->track_PIDA_kde));
+  intree->SetBranchAddress("track_PIDA_median"     ,&(varstoset->track_PIDA_median));
   intree->SetBranchAddress("track_Chi2Muon", &(varstoset->track_chi2mu_plane2));
   intree->SetBranchAddress("track_Chi2Proton", &(varstoset->track_chi2p_plane2));
   intree->SetBranchAddress("track_Chi2Pion", &(varstoset->track_chi2pi_plane2));
@@ -94,13 +97,13 @@ void settreevars(TTree *intree, treevars *varstoset){
   varstoset->track_Lp_0to1 = new std::vector<double>(nplanes);
   varstoset->track_Lmumip_0to1 = new std::vector<double>(nplanes);
   varstoset->track_Lmumippi_0to1 = new std::vector<double>(nplanes);
-
+  varstoset->track_Lmumip_0to1_nopionkaon = new std::vector<double>(nplanes);
   varstoset->track_depE_minus_rangeE_mu = new std::vector<double>(nplanes);
   varstoset->track_depE_minus_rangeE_p = new std::vector<double>(nplanes);
   varstoset->track_chi2_muminusp = new std::vector<double>(nplanes);
 }
 
-void CalcPIDvars(treevars *vars){
+void CalcPIDvars(treevars *vars, bool isScale){
   //std::cout << "Calculating PID variables for " << vars->track_likelihood_fwd_p->size() << " planes" << std::endl;
   for (size_t i_pl=0; i_pl < vars->track_likelihood_fwd_p->size(); i_pl++){
     if (i_pl==0 || i_pl==1){
@@ -118,26 +121,39 @@ void CalcPIDvars(treevars *vars){
       vars->track_chi2_muminusp->at(i_pl) = vars->track_chi2mu_plane2 - vars->track_chi2p_plane2;
     }
 
-    vars->track_likelihood_p->at(i_pl) = std::max(vars->track_likelihood_fwd_p->at(i_pl)     , vars->track_likelihood_bwd_p->at(i_pl));
-    vars->track_likelihood_mu->at(i_pl) = std::max(vars->track_likelihood_fwd_mu->at(i_pl)    , vars->track_likelihood_bwd_mu->at(i_pl));
-    vars->track_likelihood_pi->at(i_pl) = std::max(vars->track_likelihood_fwd_pi->at(i_pl)    , vars->track_likelihood_bwd_pi->at(i_pl));
-    vars->track_likelihood_k->at(i_pl) = std::max(vars->track_likelihood_fwd_k->at(i_pl)     , vars->track_likelihood_bwd_k->at(i_pl));
-    vars->track_likelihood_mip->at(i_pl) = vars->track_likelihood_fwd_mip->at(i_pl);
+    //double scalefactor_mu = 0.821;
+    //double scalefactor_p  = 0.913;
+    double scalefactor_mu = 1.0;
+    double scalefactor_p  = 1.0;
+
+    if (!isScale) {
+      scalefactor_mu = 1;
+      scalefactor_p = 1;
+    }
+
+    vars->track_likelihood_p->at(i_pl) = std::max(vars->track_likelihood_fwd_p->at(i_pl)     , vars->track_likelihood_bwd_p->at(i_pl)) * scalefactor_p;
+    vars->track_likelihood_mu->at(i_pl) = std::max(vars->track_likelihood_fwd_mu->at(i_pl)    , vars->track_likelihood_bwd_mu->at(i_pl)) * scalefactor_mu;
+    vars->track_likelihood_pi->at(i_pl) = std::max(vars->track_likelihood_fwd_pi->at(i_pl)    , vars->track_likelihood_bwd_pi->at(i_pl)) * scalefactor_mu;
+    vars->track_likelihood_k->at(i_pl) = std::max(vars->track_likelihood_fwd_k->at(i_pl)     , vars->track_likelihood_bwd_k->at(i_pl)) * scalefactor_mu;
+    vars->track_likelihood_mip->at(i_pl) = vars->track_likelihood_fwd_mip->at(i_pl) * scalefactor_mu;
     vars->track_likelihood_maxmumip->at(i_pl) = std::max(vars->track_likelihood_mu->at(i_pl), vars->track_likelihood_mip->at(i_pl));
     vars->track_likelihood_muoverp->at(i_pl) = vars->track_likelihood_mu->at(i_pl) / vars->track_likelihood_p->at(i_pl);
     vars->track_likelihood_mipoverp->at(i_pl) = vars->track_likelihood_mip->at(i_pl) / vars->track_likelihood_p->at(i_pl);
     vars->track_likelihood_maxmumipoverp->at(i_pl) = vars->track_likelihood_maxmumip->at(i_pl) / vars->track_likelihood_p->at(i_pl);
 
-    vars->track_Lmu_0to1->at(i_pl) = vars->track_likelihood_mu->at(i_pl)/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
-    vars->track_Lmip_0to1->at(i_pl) = vars->track_likelihood_mip->at(i_pl)/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
-    vars->track_Lpi_0to1->at(i_pl) = vars->track_likelihood_pi->at(i_pl)/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
-    vars->track_Lp_0to1->at(i_pl) = vars->track_likelihood_p->at(i_pl)/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
+    double denom = (vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl)); 
 
-    vars->track_Lmumip_0to1->at(i_pl) = (vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl))/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
-    vars->track_Lmumippi_0to1->at(i_pl) = (vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl)+vars->track_likelihood_pi->at(i_pl))/(vars->track_likelihood_p->at(i_pl)+vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_k->at(i_pl)+vars->track_likelihood_pi->at(i_pl)+vars->track_likelihood_mip->at(i_pl));
-
+    vars->track_Lmu_0to1->at(i_pl) = vars->track_likelihood_mu->at(i_pl)/denom;
+    vars->track_Lmip_0to1->at(i_pl) = vars->track_likelihood_mip->at(i_pl)/denom;
+    vars->track_Lpi_0to1->at(i_pl) = vars->track_likelihood_pi->at(i_pl)/denom;
+    vars->track_Lp_0to1->at(i_pl) = vars->track_likelihood_p->at(i_pl)/denom;
+    vars->track_Lmumip_0to1->at(i_pl) = (vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl))/denom;
+    vars->track_Lmumippi_0to1->at(i_pl) = (vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl)+vars->track_likelihood_pi->at(i_pl))/denom;
     vars->track_depE_minus_rangeE_mu->at(i_pl) = vars->track_depE->at(i_pl) - vars->track_rangeE_mu;
     vars->track_depE_minus_rangeE_p->at(i_pl) = vars->track_depE->at(i_pl) - vars->track_rangeE_p;
+
+    vars->track_Lmumip_0to1_nopionkaon->at(i_pl) = (vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl))/(vars->track_likelihood_mu->at(i_pl)+vars->track_likelihood_mip->at(i_pl)+vars->track_likelihood_p->at(i_pl));
+
   }
 }
 
@@ -184,64 +200,64 @@ struct hist1D{
   }
 };
 
-  void FillHist(hist1D *hists, double value, int pdg){
-    // Fill "all" histogram for every entry
-    hists->h_all->Fill(value);
+void FillHist(hist1D *hists, double value, int pdg){
+  // Fill "all" histogram for every entry
+  hists->h_all->Fill(value);
 
-    // Now fill histograms by particle type
-    if (TMath::Abs(pdg)==13){ // muon
-      hists->h_mu->Fill(value);
-    }
-    else if (TMath::Abs(pdg)==2212){ // proton
-      hists->h_p->Fill(value);
-    }
-    else if (TMath::Abs(pdg)==211){ // pion
-      hists->h_pi->Fill(value);
-    }
-    else if (TMath::Abs(pdg)==321){ // kaon
-      hists->h_k->Fill(value);
-    }
-    else{ // other
-      hists->h_other->Fill(value);
-    }
+  // Now fill histograms by particle type
+  if (TMath::Abs(pdg)==13){ // muon
+    hists->h_mu->Fill(value);
   }
+  else if (TMath::Abs(pdg)==2212){ // proton
+    hists->h_p->Fill(value);
+  }
+  else if (TMath::Abs(pdg)==211){ // pion
+    hists->h_pi->Fill(value);
+  }
+  else if (TMath::Abs(pdg)==321){ // kaon
+    hists->h_k->Fill(value);
+  }
+  else{ // other
+    hists->h_other->Fill(value);
+  }
+}
 
-  void DrawMC(hist1D *hists, double POTScaling){
-    if (POTScaling == 0.){ // area normalise
-      POTScaling = 1./hists->h_all->Integral();
-      hists->h_all->GetYaxis()->SetTitle("No. tracks (area normalised)");
-    }
-    else hists->h_all->GetYaxis()->SetTitle("No. tracks (POT normalised)");
+void DrawMC(hist1D *hists, double POTScaling){
+  if (POTScaling == 0.){ // area normalise
+    POTScaling = 1./hists->h_all->Integral();
+    hists->h_all->GetYaxis()->SetTitle("No. tracks (area normalised)");
+  }
+  else hists->h_all->GetYaxis()->SetTitle("No. tracks (POT normalised)");
 
-    hists->h_mu->Sumw2();
-    hists->h_p->Sumw2();
-    hists->h_pi->Sumw2();
-    hists->h_k->Sumw2();
-    hists->h_other->Sumw2();
-    hists->h_all->Sumw2();
+  hists->h_mu->Sumw2();
+  hists->h_p->Sumw2();
+  hists->h_pi->Sumw2();
+  hists->h_k->Sumw2();
+  hists->h_other->Sumw2();
+  hists->h_all->Sumw2();
 
-    hists->h_mu->Scale(POTScaling);
-    hists->h_p->Scale(POTScaling);
-    hists->h_pi->Scale(POTScaling);
-    hists->h_k->Scale(POTScaling);
-    hists->h_other->Scale(POTScaling);
-    hists->h_all->Scale(POTScaling);
+  hists->h_mu->Scale(POTScaling);
+  hists->h_p->Scale(POTScaling);
+  hists->h_pi->Scale(POTScaling);
+  hists->h_k->Scale(POTScaling);
+  hists->h_other->Scale(POTScaling);
+  hists->h_all->Scale(POTScaling);
 
-    std::cout << "h_all MC->Integral() = " << hists->h_all->Integral() << std::endl;
+  std::cout << "h_all MC->Integral() = " << hists->h_all->Integral() << std::endl;
 
-    THStack *hs = new THStack("hs","hs");
-    hs->Add(hists->h_p);
-    hs->Add(hists->h_mu);
-    hs->Add(hists->h_pi);
-    hs->Add(hists->h_k);
-    hs->Add(hists->h_other);
+  THStack *hs = new THStack("hs","hs");
+  hs->Add(hists->h_p);
+  hs->Add(hists->h_mu);
+  hs->Add(hists->h_pi);
+  hs->Add(hists->h_k);
+  hs->Add(hists->h_other);
 
-    hists->h_all->SetMaximum(hists->h_all->GetMaximum()*1.2);
-    hists->h_all->SetMinimum(0);
-    hists->h_all->Draw("hist"); // Draw this one first because it knows about the axis titles
-    hs->Draw("same hist");
-    hists->h_all->Draw("same E2"); // Draw it again so errors are on top
-    hists->l->Draw();
+  hists->h_all->SetMaximum(hists->h_all->GetMaximum()*1.2);
+  hists->h_all->SetMinimum(0);
+  hists->h_all->Draw("hist"); // Draw this one first because it knows about the axis titles
+  hs->Draw("same hist");
+  hists->h_all->Draw("same E2"); // Draw it again so errors are on top
+  hists->l->Draw();
 }
 
 void DrawMCPlusOffbeam(hist1D *hists, hist1D *offbeam, double POTScaling, double OffBeamScaling){
@@ -319,19 +335,133 @@ void OverlayOnBeamData(TCanvas *c, hist1D *onbeam){
   l->AddEntry(onbeam->h_all,"Data (on-beam)","lp");
 }
 
+/**
+ * Function to perform template fit and draw result to canvas
+ */
+void TemplateFit(hist1D* mchists, hist1D* onb_hists, hist1D* offb_hists, double offbeamscaling, double POTscaling){
+
+  /** Scale offbeam to onbeam */
+  offb_hists->h_all->Sumw2();
+  offb_hists->h_all->Scale(offbeamscaling);
+
+  /** create on-beam minus off-beam */
+  TH1D* data = (TH1D*)onb_hists->h_all->Clone("data");
+  data->Add(offb_hists->h_all, -1);
+
+  /** check kaons exist in sample */
+  bool isK = true;
+  if (mchists->h_k->Integral() == 0) isK = false;
+
+  /** get current fractions */
+  double mufrac = mchists->h_mu->Integral()/mchists->h_all->Integral();
+  double pfrac = mchists->h_p->Integral()/mchists->h_all->Integral();
+  double pifrac = mchists->h_pi->Integral()/mchists->h_all->Integral();
+  double kfrac = 0;
+  if (isK) kfrac = mchists->h_k->Integral()/mchists->h_all->Integral();
+  double otherfrac = mchists->h_other->Integral()/mchists->h_all->Integral();
+  double fracfloat = 0.5;
+
+  /** scale mchists to POT normally */
+  mchists->h_mu->Sumw2();
+  mchists->h_p->Sumw2();
+  mchists->h_pi->Sumw2();
+  if (isK) mchists->h_k->Sumw2();
+  mchists->h_other->Sumw2();
+
+  mchists->h_mu->Scale(POTscaling);
+  mchists->h_p->Scale(POTscaling);
+  mchists->h_pi->Scale(POTscaling);
+  if (isK) mchists->h_k->Scale(POTscaling);
+  mchists->h_other->Scale(POTscaling);
+
+  /** put mchists objects in to TObjArray */
+  TObjArray *mc = new TObjArray(4);
+  mc->Add(mchists->h_mu);
+  mc->Add(mchists->h_p);
+  mc->Add(mchists->h_pi);
+  mc->Add(mchists->h_other);
+  if (isK) mc->Add(mchists->h_k);
+  
+  /** call fitting function */
+  TFractionFitter* fit = new TFractionFitter(data, mc);
+
+  fit->Constrain(0, mufrac*fracfloat, mufrac*(1+fracfloat));
+  fit->Constrain(1, pfrac*fracfloat, pfrac*(1+fracfloat));
+  fit->Constrain(2, pifrac*fracfloat, pifrac*(1+fracfloat));
+  fit->Constrain(3, otherfrac*fracfloat, otherfrac*(1+fracfloat));
+  if (isK) fit->Constrain(4, kfrac*fracfloat, kfrac*(1+fracfloat));
+  
+  int stat = fit->Fit();
+
+  double result_mu = 0;
+  double result_mu_err = 0;
+  fit->GetResult(0,result_mu, result_mu_err);
+
+  double result_p = 0;
+  double result_p_err = 0;
+  fit->GetResult(1,result_p, result_p_err);
+
+  double result_pi = 1;
+  double result_pi_err = 0;
+  fit->GetResult(2,result_pi, result_pi_err);
+
+  double result_other = 0;
+  double result_other_err = 0;
+  fit->GetResult(3,result_other, result_other_err);
+
+  double result_k = 0;
+  double result_k_err = 0;
+  if (isK) fit->GetResult(4,result_k, result_k_err);
+
+  std::cout << "[TemplateFit] mu    : " << result_mu << "+/-" << result_mu_err << std::endl;
+  std::cout << "[TemplateFit] p     : " << result_p << "+/-" << result_p_err << std::endl;
+  std::cout << "[TemplateFit] pi    : " << result_pi << "+/-" << result_pi_err << std::endl;
+  std::cout << "[TemplateFit] k     : " << result_k << "+/-" << result_k_err << std::endl;
+  std::cout << "[TemplateFit] other : " << result_other << "+/-" << result_other_err << std::endl;
+
+  /** 
+   * draw result: because data histograms modified directly just 
+   * draw them in this function rather than drawing using the function
+   */
+
+  mchists->h_mu->Scale(result_mu * (data->Integral()/mchists->h_mu->Integral()));
+  mchists->h_p->Scale(result_p * (data->Integral()/mchists->h_p->Integral()));
+  mchists->h_pi->Scale(result_pi * (data->Integral()/mchists->h_pi->Integral()));
+  if (isK) mchists->h_k->Scale(result_k * (data->Integral()/mchists->h_k->Integral()));
+  mchists->h_other->Scale(result_other * (data->Integral()/mchists->h_other->Integral()));
+
+  /*TH1D* hTotal = (TH1D*)mchists->h_mu->Clone("hTotal");
+  hTotal->Add(mchists->h_p);
+  hTotal->Add(mchists->h_pi);
+  hTotal->Add(mchists->h_k);
+  hTotal->Add(mchists->h_other);
+*/
+  THStack* hs = new THStack();
+  hs->Add(mchists->h_p);
+  hs->Add(mchists->h_mu);
+  hs->Add(mchists->h_pi);
+  if (isK) hs->Add(mchists->h_k);
+  hs->Add(mchists->h_other);
+
+  //hTotal->Draw();
+ 
+  hs->Draw("hist");
+
+  data->Draw("same");
+}
 
 void DrawMCEffPur(TCanvas *c, hist1D *hists, bool MIPlow){
   std::vector<TH1D*> histstoeval = {
-                    hists->h_mu,
-                    hists->h_pi,
-                    hists->h_p
-                    };
+    hists->h_mu,
+    hists->h_pi,
+    hists->h_p
+  };
 
   std::vector<std::string> histtitles = {
-                    "True muons",
-                    "True pions",
-                    "True protons"
-                    };
+    "True muons",
+    "True pions",
+    "True protons"
+  };
 
   c->Divide(2,2,0.0005,0.0005);
 
@@ -385,17 +515,17 @@ void DrawMCEffPur(TCanvas *c, hist1D *hists, bool MIPlow){
     heff->SetMarkerColor(kRed);
     heff->SetMarkerStyle(20);
     heff->SetMarkerSize(.3
-    );
+        );
     hpur->SetLineColor(kBlue);
     hpur->SetMarkerColor(kBlue);
     hpur->SetMarkerStyle(20);
     hpur->SetMarkerSize(.3
-    );
+        );
     heffpur->SetLineColor(kBlack);
     heffpur->SetMarkerColor(kBlack);
     heffpur->SetMarkerStyle(20);
     heffpur->SetMarkerSize(.3
-    );
+        );
 
     heff->GetYaxis()->SetRangeUser(0,1.1);
 
