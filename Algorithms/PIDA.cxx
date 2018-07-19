c616f8c85cc5a9a6745301eba0c97fffc316c93c
kduffy <kduffy@fnal.gov>
2018-07-19 16:52:10 -0500

    Restrict PIDA to last 30cm of track (resRange<=30) only

--- Algorithms/PIDA.cxx
+++ Algorithms/PIDA.cxx
@@ -14,12 +14,14 @@ namespace particleid{
     std::vector<double> pidaValues;
     if (resRange.size() == 0 || resRange.size() == 1) return -1;
     for (size_t i = 1; i < resRange.size()-1; i++){
-
+      // Don't take resRange values higher than 30cm
+      if (resRange.at(i)>30) continue;
+      
       pidaValues.push_back(dEdx.at(i)*std::pow(resRange.at(i),0.42));
 
     }
 
-    if (method == "mean") 
+    if (method == "mean")
       pida = TMath::Mean(pidaValues.begin(), pidaValues.end());
 
     else if (method == "median")
