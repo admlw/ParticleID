31d89d1af69a5cf01b47a028ef79ea38bf8a2404
kduffy <kduffy@fnal.gov>
2018-07-20 10:47:55 -0500

    Fix bug where I had the bits reversed, and clean up error messages a bit.

--- Algorithms/uB_PlaneIDBitsetHelperFunctions.h
+++ Algorithms/uB_PlaneIDBitsetHelperFunctions.h
@@ -4,22 +4,25 @@
 
 namespace UBPID{
 
+  /// Returns planeID from bitset, if exactly 1 plane is set in the bitset.
+  /// Plane 2 = collection (y) plane, Plane 1 = induction (v) plane, Plane 0 = induction (u) plane
   inline int uB_getSinglePlane(std::bitset<5> planeid){
-    if ((planeid[0]+planeid[1]+planeid[2]+planeid[3]+planeid[4]>1) || (planeid[0]+planeid[1]+planeid[2]+planeid[3]+planeid[4]==0) || planeid[3]==1 || planeid[4]==1){
-      std::cout << "[ParticleID::PlaneIDBitsetHelperFunctions] Cannot return a single MicroBooNE plane for bitset " << planeid << ". Returning -1 (invalid planeID). This may cause you problems!" << std::endl;
+    if ((planeid[0]+planeid[1]+planeid[2]+planeid[3]+planeid[4]>1) || (planeid[0]+planeid[1]+planeid[2]+planeid[3]+planeid[4]==0) || planeid[0]==1 || planeid[1]==1){
+      std::cout << "[uB_PlaneIDBitsetHelper] Cannot return a single MicroBooNE plane for bitset " << planeid << ". Returning -1 (invalid planeID)." << std::endl;
       return -1;
     }
-    else if (planeid[0]==1) return 2;
-    else if (planeid[1]==1) return 1;
+    else if (planeid[4]==1) return 2;
+    else if (planeid[3]==1) return 1;
     else if (planeid[2]==1) return 0;
 
     // Default: invalid return
     return -1;
   }
 
+  /// Returns the bitset corresponding to a given (single) uBooNE plane.
   inline std::bitset<5> uB_SinglePlaneGetBitset(int planeid){
     if (planeid<0 || planeid>2){
-      std::cout << "[ParticleID::PlaneIDBitsetHelperFunctions] Cannot return a bitset for MicroBooNE planeid " << planeid << ". Returning 00000 (invalid planeID). This may cause you problems!" << std::endl;
+      std::cout << "[uB_PlaneIDBitsetHelper] Cannot return a bitset for MicroBooNE planeid " << planeid << ". Returning 00000 (invalid planeID)." << std::endl;
       return std::bitset<5>("00000");
     }
     else if (planeid==2) return std::bitset<5>("10000");
