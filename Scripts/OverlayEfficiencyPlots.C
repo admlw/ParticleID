b8c772f6478ba75f4da019f6bdad67ba61b0d107
Adam <a.lister1@lancaster.ac.uk>
2019-01-12 22:01:37 -0600

    making overlays a little cleaner

--- Scripts/OverlayEfficiencyPlots.C
+++ Scripts/OverlayEfficiencyPlots.C
@@ -1,5 +1,5 @@
 std::vector<int> marker_styles = {20,24,34};
-std::vector<double> marker_sizes = {0.4,0.4,0.5};
+std::vector<double> marker_sizes = {0.4,0.4,0.4};
 
 void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std::string> filedescriptions={}){
 
@@ -43,11 +43,11 @@ void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std
   // }
 
   // Now we have a list of all the plots we want to overlay, let's get going!
-  std::vector<std::string> particles = {"_mu","_pi","_p","_k"};
+  std::vector<std::string> particles = {"_mu","_pi","_p"}; //,"_k"};
   // Loop over plots
   for (size_t i_plot=0; i_plot<plotnames.size(); i_plot++){
 
-    TCanvas *c1 = new TCanvas();
+    TCanvas *c1 = new TCanvas("c1", "c1", 600, 400);
     c1->Divide(2,2,0.0005,0.0005);
 
     TLegend *l;
@@ -76,6 +76,37 @@ void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std
         hpur->SetDirectory(0);
         heffpur->SetDirectory(0);
 
+        int markerstyle = 20;
+        if (marker_styles.size() < i_file) markerstyle = marker_styles.at(marker_styles.size()-1)+(i_file - marker_styles.size());
+        else markerstyle = marker_styles.at(i_file);
+
+        double markersize = 0.3;
+        if (marker_sizes.size() >= i_file) markersize = marker_sizes.at(i_file);
+
+        heff->SetFillStyle(0);
+        heff->SetLineColor(kGreen+1);
+        heff->SetLineWidth(1);
+        heff->SetMarkerColor(kGreen+1);
+        heff->SetMarkerStyle(markerstyle);
+        heff->SetMarkerSize(markersize);
+
+        hpur->SetFillStyle(0);
+        hpur->SetLineColor(kAzure+1);
+        hpur->SetLineWidth(1);
+        hpur->SetMarkerColor(kAzure+1);
+        hpur->SetMarkerStyle(markerstyle);
+        hpur->SetMarkerSize(markersize);
+
+        heffpur->SetFillStyle(0);
+        heffpur->SetLineColor(kBlack);
+        heffpur->SetLineWidth(1);
+        heffpur->SetMarkerColor(kBlack);
+        heffpur->SetMarkerStyle(markerstyle);
+        heffpur->SetMarkerSize(markersize);
+
+        heff->GetYaxis()->SetRangeUser(0,1.1);
+
+
         if (i_p==1 && i_file==0){
           double legendy=0.;
           double legendpts=0.;
@@ -87,11 +118,13 @@ void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std
           }
 
           legendy = legendy/legendpts;
-          if (legendy<0.6) l = new TLegend(0.5,0.6,0.82,0.88);
-          else l = new TLegend(0.5,0.3,0.82,0.6);
-          l->SetTextFont(132);
+          if (legendy<0.6) l = new TLegend(0.12,0.3,1.0,0.88);
+          else l = new TLegend(0.12,0.3,1.0,0.88);
+          //l->SetTextFont(132);
           l->SetLineColor(kWhite);
           l->SetFillColor(kWhite);
+          l->SetTextSize(0.06);
+          l->SetBorderSize(0);
 
           TH1D *heff_clone = (TH1D*)heff->Clone("heff_clone");
           TH1D *hpur_clone = (TH1D*)hpur->Clone("hpur_clone");
@@ -105,36 +138,6 @@ void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std
           l->AddEntry(hpur_clone,"Purity","l");
           l->AddEntry(heffpur_clone,"Efficiency #times Purity","l");
         }
-
-        int markerstyle = 20;
-        if (marker_styles.size() < i_file) markerstyle = marker_styles.at(marker_styles.size()-1)+(i_file - marker_styles.size());
-        else markerstyle = marker_styles.at(i_file);
-
-        double markersize = 0.3;
-        if (marker_sizes.size() >= i_file) markersize = marker_sizes.at(i_file);
-
-        heff->SetFillStyle(0);
-        heff->SetLineColor(kRed);
-        heff->SetLineWidth(1);
-        heff->SetMarkerColor(kRed);
-        heff->SetMarkerStyle(markerstyle);
-        heff->SetMarkerSize(markersize);
-
-        hpur->SetFillStyle(0);
-        hpur->SetLineColor(kBlue);
-        hpur->SetLineWidth(1);
-        hpur->SetMarkerColor(kBlue);
-        hpur->SetMarkerStyle(markerstyle);
-        hpur->SetMarkerSize(markersize);
-
-        heffpur->SetFillStyle(0);
-        heffpur->SetLineColor(kBlack);
-        heffpur->SetLineWidth(1);
-        heffpur->SetMarkerColor(kBlack);
-        heffpur->SetMarkerStyle(markerstyle);
-        heffpur->SetMarkerSize(markersize);
-
-        heff->GetYaxis()->SetRangeUser(0,1.1);
 
         c1->cd(i_p+1);
         if (i_file==0){
@@ -167,7 +170,7 @@ void OverlayEfficiencyPlots(std::vector<std::string> inputfiles, std::vector<std
 
     } // end loop over input files (i_file)
 
-    c1->cd(2);
+    c1->cd(4);
     l->Draw();
     c1->Print(TString::Format("%s.png",plotnames.at(i_plot).c_str()).Data());
     delete c1;
